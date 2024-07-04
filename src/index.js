3d0a5c8f9684ecec2fd3328ab52d1c7851a53f9b
pate1595 <kmp.patel@utoronto.ca>
2024-07-03 20:58:57 -0400

    add portfolio src files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Hi, I'm Krish!</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
