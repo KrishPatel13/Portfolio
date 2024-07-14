ba51edeeee45e12523f58995700573a1f00f69d4
pate1595 <kmp.patel@utoronto.ca>
2024-07-13 20:18:05 -0400

    Improve the about me section

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,52 +3,47 @@ import Heading from './Heading.vue';
 </script>
 
 <template>
+    <img
+        class="background-image"
+        src="/assets/images/profile11.jpg"
+    />
+
     <div id="about">
         <img
             class="profile-image"
-            src="/assets/images/profile12.jpg"
-            alt=""
+            src="/assets/images/profile.png"
+            alt="krish's profile image"
             data-aos="zoom-in"
         />
-        <img
-            class="background-image"
-            src="/assets/images/profile11.jpg"
-        />
+
         <div class="glass-overlay"></div>
+
         <div>
             <Heading subtle-text="About" emphasized-text="Me" />
             <div class="description" data-aos="zoom-out-up">
-                I am a <strong>Software Engineer</strong> based in <strong>New York City</strong> 
-                working for <strong>Figma</strong>. I've been working full time in the industry since 
-                July 2023 but prior to this, studied <strong>Computer Science</strong> at the 
-                <strong>University of Toronto</strong> with an emphasis on <strong>Software Engineering</strong>.
-                During my time there, I obtained <strong>1+ year(s) of experience</strong> interning 
-                across a few software companies in Canada. I’ve also helped build/teach a few courses
-                surrounding low-level concepts in computer science. Currently, I
-                have an interest in <strong>distributed systems</strong> and <strong>cybersecurity</strong>
-                but ultimately wish to make an impact within the industry! To learn more about me or
-                keep in touch, keep scrolling!
+                As a recent graduate from the University of Toronto with an Honours Bachelor of Science in Computer Science and a minor in Mathematical Science.
+                I am passionate about <strong>Artificial Intelligence</strong> and its potential to revolutionize technology.
+                My professional experience includes working as a <strong>Database Software Engineer</strong> at Huawei Technologies and a Machine Learning Engineer at MLDSAI Inc., where I contributed to the development of AI-driven solutions.
+                Additionally, I have served as a <strong>Teaching Assistant</strong> for various computer science and mathematics courses, honing my skills in both teaching and technical domains. I am eager to further explore AI through a master's program, aiming to contribute to <strong>cutting-edge advancements</strong> in this exciting field.
             </div>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-@import url('/assets/styles/utils.css');
-@import url('/assets/styles/profile-image.css');  /* Import profile.css here */
+    @import url('/assets/styles/utils.css');
+    @import url('/assets/styles/profile-image.css');
 
-#about {
-    display: flex;
-
-    @media (max-width: 716px) {
-        flex-direction: column;
-        align-items: center;
+    #about {
+        display: flex;
+        margin-top: 15px;
+        @media (max-width: 716px) {
+            flex-direction: column;
+            align-items: center;
+        }
     }
-}
 
-
-
-.description {
-    font-size: 18px;
-}
+    .description {
+        font-size: 18px;
+    }
 </style>
