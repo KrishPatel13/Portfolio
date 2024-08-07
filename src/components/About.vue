1b7e15fd5fa053e4ef7317eaa2fec2e458899c5d
pate1595 <kmp.patel@utoronto.ca>
2024-08-07 12:42:18 -0400

    chore: Add favicon and update HTML structure for favicon in index.html

--- src/components/About.vue
+++ src/components/About.vue
@@ -16,13 +16,19 @@ import Heading from './Heading.vue';
         <div>
             <Heading subtle-text="About" emphasized-text="Me" />
             <div class="description" data-aos="zoom-out-up">
-                I am Krish Patel, currently pursuing an <strong>Honours Bachelor of Science</strong> in <strong>Computer Science</strong> at the <strong>University of Toronto</strong>, with a strong academic focus complemented by practical experience. My journey through the world of technology has been shaped by hands-on roles in <strong>software engineering</strong>, <strong>machine learning</strong>, and <strong>database technologies</strong>.
+                I am Krish Patel, currently pursuing an <strong>Honours Bachelor of Science</strong> in <strong>Computer Science</strong> at the <strong>University of Toronto</strong>, with a strong academic focus complemented by practical experience.<br><br>
+            </div>
 
-                My professional experience includes a <strong>Software Engineer Intern</strong> position at <strong>Huawei Technologies Canada</strong>, where I worked on <strong>distributed databases</strong>, optimizing system performance and automating critical processes. I also served as a <strong>Machine Learning Engineer Intern</strong> at <strong>MLDSAI Inc.</strong>, contributing to the development of <strong>AI-driven automation solutions</strong> that significantly improved operational efficiency.
+            <div class="description" data-aos="zoom-out-up" style="text-indent: 4em;">
+                My professional journey spans several key areas: <strong>Machine Learning</strong>, <strong>Artificial Intelligence</strong>, <strong>Database Technologies</strong>, <strong>Low-level Programming</strong>, and <strong>Full-stack Development</strong>.<br><br>
+            </div>
 
-                Beyond industry experience, I have a solid background in academia as a <strong>Teaching Assistant</strong> for various <strong>computer science</strong> and <strong>mathematics</strong> courses at the University of Toronto. This role has allowed me to refine my <strong>technical expertise</strong> and develop strong <strong>teaching</strong> and <strong>mentoring skills</strong>.
+            <div class="description" data-aos="zoom-out-up" style="text-indent: 4em;">
+                Beyond industry experience, I have a solid background in academia as a <strong>Teaching Assistant</strong> for various <strong>computer science</strong> and <strong>mathematics</strong> courses at the University of Toronto. This role has allowed me to refine my <strong>technical expertise</strong> and develop strong <strong>teaching</strong> and <strong>mentoring skills</strong>.<br><br>
+            </div>
 
-                Driven by a passion for <strong>Artificial Intelligence</strong> and its transformative potential, I am eager to deepen my knowledge and contribute to <strong>innovative advancements</strong> in this rapidly evolving field.
+            <div class="description" data-aos="zoom-out-up">
+                Driven by a passion for <strong>Software Engineering</strong>, <strong>Machine Learning</strong>, and <strong>Artificial Intelligence</strong> and its transformative potential, I am eager to deepen my knowledge and contribute to <strong>innovative advancements</strong> in this rapidly evolving field that brings impact in society.
             </div>
         </div>
     </div>
