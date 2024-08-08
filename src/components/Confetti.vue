d98083ffbed225d2e6dd4d26cc1f7b5eb88a3d48
pate1595 <kmp.patel@utoronto.ca>
2024-08-07 22:29:04 -0400

    feat: Add canvas-confetti library for confetti animation

---
+++ src/components/Confetti.vue
@@ -0,0 +1,37 @@
+<template>
+	<div @click="triggerConfetti" class="confetti-wrapper">
+		<slot></slot>
+	</div>
+</template>
+
+<script setup lang="ts">
+import confetti from 'canvas-confetti';
+
+const pointedOvalPath = "M10 20 Q 25 10 40 20 Q 25 30 10 20 Z";
+var pointedOvalShape = confetti.shapeFromPath({ path: pointedOvalPath });
+
+const triggerConfetti = (event) => {
+	const x = event.clientX / window.innerWidth;
+	const y = event.clientY / window.innerHeight;
+
+	// Trigger the custom-shaped confetti at the click position
+	confetti({
+		particleCount: 3,
+		spread: 360,
+		startVelocity: 5, 
+		origin: { x, y },
+		shapes: [pointedOvalShape],
+		scalar: 2,
+	});
+
+	setTimeout(() => {
+		confetti.reset();
+	}, 1000);
+};
+</script>
+
+<style scoped>
+.confetti-wrapper {
+	position: relative;
+}
+</style>
