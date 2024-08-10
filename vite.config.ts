7bb044ddbe2ee64616235bbe7aa762bcae713796
pate1595 <kmp.patel@utoronto.ca>
2024-08-10 00:36:11 -0400

    chore: trying to fix lighthouse performace stats

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,19 @@ import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import compression from 'vite-plugin-compression';
+import viteCompression from 'vite-plugin-compression';
+import purgecss from 'vite-plugin-purgecss';
 
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    compression({
-      algorithm: 'gzip',
-      ext: '.gz',
-    }),],
+    viteCompression(),
+    purgecss({
+      content: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx', './src/**/*.tsx'], // specify the paths to your files
+      safelist: ['html', 'body'], // add any classes that should not be removed
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -22,4 +25,4 @@ export default defineConfig({
     '__VUE_PROD_DEVTOOLS__': false, // or true if you want to enable Vue Devtools in production
     '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': false, // or true if you need this feature
   }
-})
+});
