d66918ec9565f6dcb0ee89d1c5ad9bc4c1a29f56
pate1595 <kmp.patel@utoronto.ca>
2024-08-13 19:02:20 -0400

    chore: Update dependencies and add PDF viewer components

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import NotFound from './components/NotFound.vue';
 import * as VueRouter from 'vue-router';
 import 'aos/dist/aos.css';
 
-export const routes = [{ path: '/:pathMatch(.*)*', component: NotFound }];
+const routes = [
+    { path: '/', component: App },
+    { path: '/:pathMatch(.*)*', component: NotFound },
+];
 
 const router = VueRouter.createRouter({
     history: VueRouter.createWebHistory(),
